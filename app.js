33a06f9a1dd440f9ff3e094c291d80cdb9baa5de
Ilkin <feyzullayevilkin10@gmail.com>
2025-07-09 16:46:57 +0300

    Adds note creation, dark mode toggle, and UI enhancements
    Implements functionality to create and display notes with title and content. Adds dark mode toggle for better user experience. Enhances UI with responsive styling, placeholder text for empty notes, and integration of Google Fonts for improved design. Fixes modal behavior for note creation and cancellation.

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
 const main = document.getElementById("notes-grid");
 const dialog = document.getElementById("dialog");
 
-const notes = [];
-
 document.addEventListener("DOMContentLoaded", function () {
-  main.innerHTML = notes.join("");
-
   const addNote = document.getElementById("add-note");
   addNote.addEventListener("click", function () {
-    // notes.push(`<h1>Hello${notes.length}</h1>`);
-    // main.innerHTML = notes.join("");
-    dialog.showModal();
+    const isOpen = dialog.getAttribute("open");
+    console.log({ isOpen });
+    if (isOpen === null) {
+      dialog.showModal();
+    } else {
+      dialog.close();
+    }
+  });
+
+  document
+    .getElementById("cancel-dialog")
+    .addEventListener("click", function () {
+      dialog.close();
+      document.getElementById("title-input").value = "";
+      document.getElementById("note-input").value = "";
+    });
+
+  document.getElementById("submit-note").addEventListener("click", function () {
+    const noteHeader = document.getElementById("title-input").value;
+    const noteContent = document.getElementById("note-input").value;
+    dialog.close();
+    main.innerHTML += `<div class="note">
+        <h3 class="note-header">${noteHeader}</h3>
+        <p class="note-content">
+          ${noteContent}
+        </p>
+      </div>`;
+    document.getElementById("title-input").value = "";
+    document.getElementById("note-input").value = "";
+    document.getElementById("no-note").innerHTML = "";
+  });
+
+  const modeChanger = document.getElementById("mode-changer");
+  modeChanger.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+    if (modeChanger.innerHTML === "🌙") {
+      modeChanger.innerHTML = "☀️";
+    } else {
+      modeChanger.innerHTML = "🌙";
+    }
   });
 });
